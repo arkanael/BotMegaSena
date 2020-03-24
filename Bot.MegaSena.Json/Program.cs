e7e54fba19bb3b6e85e65074943f9a607fdcbe0d
Luiz Guilherme Bandeira <arkanael@gmail.com>
2020-03-24 06:59:41 -0300

    Add project files.

---
+++ Bot.MegaSena.Json/Program.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot.MegaSena.Json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url;
+
+            Console.WriteLine("Informe o número do concurso.");
+            string numeroConcurso = Console.ReadLine();
+
+            string html;
+
+            if (string.IsNullOrWhiteSpace(numeroConcurso))
+            {
+                url = @"http://loterias.caixa.gov.br/wps/portal/loterias/landing/megasena/!ut/p/a1/04_Sj9CPykssy0xPLMnMz0vMAfGjzOLNDH0MPAzcDbwMPI0sDBxNXAOMwrzCjA0sjIEKIoEKnN0dPUzMfQwMDEwsjAw8XZw8XMwtfQ0MPM2I02-AAzgaENIfrh-FqsQ9wNnUwNHfxcnSwBgIDUyhCvA5EawAjxsKckMjDDI9FQE-F4ca/dl5/d5/L2dBISEvZ0FBIS9nQSEh/pw/Z7_HGK818G0KO6H80AU71KG7J0072/res/id=buscaResultado/c=cacheLevelPage/=/?timestampAjax=1581042977734";
+
+            }
+            else
+            {
+                url = @"http://loterias.caixa.gov.br/wps/portal/loterias/landing/megasena/!ut/p/a1/04_Sj9CPykssy0xPLMnMz0vMAfGjzOLNDH0MPAzcDbwMPI0sDBxNXAOMwrzCjA0sjIEKIoEKnN0dPUzMfQwMDEwsjAw8XZw8XMwtfQ0MPM2I02-AAzgaENIfrh-FqsQ9wNnUwNHfxcnSwBgIDUyhCvA5EawAjxsKckMjDDI9FQE-F4ca/dl5/d5/L2dBISEvZ0FBIS9nQSEh/pw/Z7_HGK818G0KO6H80AU71KG7J0072/res/id=buscaResultado/c=cacheLevelPage/=/?timestampAjax=1581042832097&concurso=" + numeroConcurso;
+            }
+            
+            string json;
+
+            using (WebClient webclient = new WebClient())
+            {
+                html = webclient.DownloadString(url);
+                json = webclient.DownloadString(url);
+            }
+
+            
+            var resultado = JsonConvert.DeserializeObject<ResultadoMegaSena>(json);
+            
+            if (resultado.mensagens.Contains("Concurso inexistente. Por favor, confira numeração digitada"))
+            {
+                Console.WriteLine("erro");
+            }
+            Console.WriteLine($"Concurso número: {resultado.concurso} ");
+            Console.WriteLine($"Resultado: {resultado.ResultadoOrdenado}");
+            Console.WriteLine($"Total de Ganhadores: {resultado.ganhadores} ");
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Informe o número do concurso.");
+            string numeroConcurso = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(numeroConcurso))
+            {
+                numeroConcurso = "2231";
+
+            }
+
+            string url = @"http://www1.caixa.gov.br/loterias/loterias/megasena/megasena_pesquisa_new.asp?submeteu=sim&opcao=concurso&txtConcurso=" + numeroConcurso;
+            string html;
+
+            using (WebClient webclient = new WebClient())
+            {
+                webclient.Headers["Cookie"] = "security=true";
+                html = webclient.DownloadString(url);
+            }
+
+            html = html.Replace("<span> class=\"num_sorteio\"><ul>", "");
+            html = html.Replace("</ul></span>", "");
+            html = html.Replace("</li>", "");
+
+            string[] vetor = Regex.Split(html, "<li>");
+            List<int> resultado = new List<int>();
+
+            resultado.Add(int.Parse(vetor[1]));
+            resultado.Add(int.Parse(vetor[2]));
+            resultado.Add(int.Parse(vetor[3]));
+            resultado.Add(int.Parse(vetor[4]));
+            resultado.Add(int.Parse(vetor[5]));
+            resultado.Add(int.Parse(vetor[6].Substring(0, 2)));
+
+            Console.WriteLine($"Concurso Selecionado: {numeroConcurso}");
+            Console.Write("Resultado: ");
+            resultado.OrderBy(x => x).ToList().ForEach(resultadoMegaSena => 
+            {
+                Console.Write($"{resultadoMegaSena} ");
+            });
+
+            Console.ReadKey();
+        }
+    }
+}
